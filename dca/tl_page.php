b8d815d739adfc49efe1848d8d5c16b462f2c2f1
Andreas Schempp <andreas@schempp.ch>
2011-06-17 10:18:55 +0000

    FolderPage: Show non-root pages in breadcrumb

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -29,7 +29,116 @@
 
 
 /**
+ * Config
+ */
+foreach( $GLOBALS['TL_DCA']['tl_page']['config']['onload_callback'] as $k => $callback )
+{
+	if ($callback[1] == 'addBreadcrumb')
+	{
+		$GLOBALS['TL_DCA']['tl_page']['config']['onload_callback'][$k][0] = 'tl_page_folderurl';
+	}
+}
+
+
+/**
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_page']['palettes']['folder'] = '{title_legend},title,type';
+
+
+class tl_page_folderurl extends Backend
+{
+	
+	/**
+	 * Override the default breadcrumb menu, we want to show pages before root pages
+	 */
+	public function addBreadcrumb()
+	{
+		// Set a new node
+		if (isset($_GET['node']))
+		{
+			$this->Session->set('tl_page_node', $this->Input->get('node'));
+			$this->redirect(preg_replace('/&node=[^&]*/', '', $this->Environment->request));
+		}
+
+		$intNode = $this->Session->get('tl_page_node');
+
+		if ($intNode < 1)
+		{
+			return;
+		}
+
+		$arrIds = array();
+		$arrLinks = array();
+
+		// Generate breadcrumb trail
+		if ($intNode)
+		{
+			$intId = $intNode;
+
+			do
+			{
+				$objPage = $this->Database->prepare("SELECT * FROM tl_page WHERE id=?")
+								->limit(1)
+								->execute($intId);
+
+				if ($objPage->numRows < 1)
+				{
+					// Currently selected page does not exits
+					if ($intId == $intNode)
+					{
+						$this->Session->set('tl_page_node', 0);
+						return;
+					}
+
+					break;
+				}
+
+				$arrIds[] = $intId;
+
+				// No link for the active page
+				if ($objPage->id == $intNode)
+				{
+					$arrLinks[] = $this->addIcon($objPage->row(), '', null, '', true) . ' ' . $objPage->title;
+				}
+				else
+				{
+					$arrLinks[] = $this->addIcon($objPage->row(), '', null, '', true) . ' <a href="' . $this->addToUrl('node='.$objPage->id) . '">' . $objPage->title . '</a>';
+				}
+
+				// Do not show the mounted pages
+				if (!$this->User->isAdmin && $this->User->hasAccess($objPage->id, 'pagemounts'))
+				{
+					break;
+				}
+
+				$intId = $objPage->pid;
+			}
+			while ($intId > 0);
+		}
+
+		// Check whether the node is mounted
+		if (!$this->User->isAdmin && !$this->User->hasAccess($arrIds, 'pagemounts'))
+		{
+			$this->Session->set('tl_page_node', 0);
+
+			$this->log('Page ID '.$intNode.' was not mounted', 'tl_page addBreadcrumb', TL_ERROR);
+			$this->redirect('contao/main.php?act=error');
+		}
+
+		// Limit tree
+		$GLOBALS['TL_DCA']['tl_page']['list']['sorting']['root'] = array($intNode);
+
+		// Add root link
+		$arrLinks[] = '<img src="system/themes/' . $this->getTheme() . '/images/pagemounts.gif" width="18" height="18" alt="" /> <a href="' . $this->addToUrl('node=0') . '">' . $GLOBALS['TL_LANG']['MSC']['filterAll'] . '</a>';
+		$arrLinks = array_reverse($arrLinks);
+
+		// Insert breadcrumb menu
+		$GLOBALS['TL_DCA']['tl_page']['list']['sorting']['breadcrumb'] .= '
+
+<ul id="tl_breadcrumb">
+  <li>' . implode(' &gt; </li><li>', $arrLinks) . '</li>
+</ul>';
+	}
+}
 
