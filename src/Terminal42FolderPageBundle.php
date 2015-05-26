bb0823438e85a477a2cd70b95dd6ac494f2ecc00
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-05-26 10:05:09 +0200

    Updated composer.json and added bundle class

---
+++ src/Terminal42FolderPageBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * folderpage extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2012-2015, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-folderpage
+ */
+
+namespace Terminal42\Folderpage;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Terminal42FolderPageBundle extends Bundle
+{
+}
