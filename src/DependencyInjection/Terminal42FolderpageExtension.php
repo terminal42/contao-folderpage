632d516de9e10b9cc6161bdab769725206598ce2
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-07-12 17:53:58 +0200

    CS

--- src/DependencyInjection/Terminal42FolderpageExtension.php
+++ src/DependencyInjection/Terminal42FolderpageExtension.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * folderpage extension for Contao Open Source CMS
- *
- * @copyright  Copyright (c) 2017, terminal42 gmbh
- * @author     terminal42 gmbh <info@terminal42.ch>
- * @license    LGPL-3.0+
- * @link       http://github.com/terminal42/contao-folderpage
- */
 
 namespace Terminal42\FolderpageBundle\DependencyInjection;
 
@@ -18,9 +10,6 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class Terminal42FolderpageExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $config, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader(
--- src/Terminal42FolderpageBundle.php
+++ src/Terminal42FolderpageBundle.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * folderpage extension for Contao Open Source CMS
- *
- * @copyright  Copyright (c) 2017, terminal42 gmbh
- * @author     terminal42 gmbh <info@terminal42.ch>
- * @license    LGPL-3.0+
- * @link       http://github.com/terminal42/contao-folderpage
- */
 
 namespace Terminal42\FolderpageBundle;
 
