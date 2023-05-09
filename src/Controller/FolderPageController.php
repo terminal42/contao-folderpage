e8c2ac392747cc0fe9391815a518c61c225edc08
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-05-09 13:28:00 +0200

    Allow folderpage in navigation

--- src/Controller/FolderPageController.php
+++ src/Controller/FolderPageController.php
@@ -5,15 +5,49 @@ declare(strict_types=1);
 namespace Terminal42\FolderpageBundle\Controller;
 
 use Contao\CoreBundle\Exception\PageNotFoundException;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Routing\Page\DynamicRouteInterface;
+use Contao\CoreBundle\Routing\Page\PageRoute;
 use Contao\CoreBundle\ServiceAnnotation\Page;
+use Contao\PageModel;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 /**
- * @Page(path=false, contentComposition=false)
+ * @Page(contentComposition=false)
  */
-class FolderPageController
+class FolderPageController implements DynamicRouteInterface
 {
-    public function __invoke(): void
+    private ContaoFramework $framework;
+
+    public function __construct(ContaoFramework $framework)
+    {
+        $this->framework = $framework;
+    }
+
+    public function __invoke(PageModel $pageModel): Response
     {
         throw new PageNotFoundException();
+    }
+
+    public function configurePageRoute(PageRoute $route): void
+    {
+        $pageModel = $route->getPageModel();
+
+        if ($pageModel->hide || !$this->getForwardPage((int) $pageModel->id)) {
+            throw new ResourceNotFoundException(sprintf('Page ID %s is not routable', $pageModel->id));
+        }
+    }
+
+    public function getUrlSuffixes(): array
+    {
+        return [];
+    }
+
+    private function getForwardPage(int $pageId): ?PageModel
+    {
+        $this->framework->initialize();
+
+        return PageModel::findFirstPublishedRegularByPid($pageId);
     }
 }
