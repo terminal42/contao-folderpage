17f50a2da2d90f43c9c14ecf872ce4c5f5920dac
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-10-24 14:19:09 +0200

    Cannot dynamically disable routing for a page controller

--- src/Controller/FolderPageController.php
+++ src/Controller/FolderPageController.php
@@ -5,49 +5,17 @@ declare(strict_types=1);
 namespace Terminal42\FolderpageBundle\Controller;
 
 use Contao\CoreBundle\Exception\PageNotFoundException;
-use Contao\CoreBundle\Framework\ContaoFramework;
-use Contao\CoreBundle\Routing\Page\DynamicRouteInterface;
-use Contao\CoreBundle\Routing\Page\PageRoute;
 use Contao\CoreBundle\ServiceAnnotation\Page;
 use Contao\PageModel;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 /**
  * @Page(contentComposition=false)
  */
-class FolderPageController implements DynamicRouteInterface
+class FolderPageController
 {
-    private ContaoFramework $framework;
-
-    public function __construct(ContaoFramework $framework)
-    {
-        $this->framework = $framework;
-    }
-
     public function __invoke(PageModel $pageModel): Response
     {
         throw new PageNotFoundException();
-    }
-
-    public function configurePageRoute(PageRoute $route): void
-    {
-        $pageModel = $route->getPageModel();
-
-        if ($pageModel->hide || !$this->getForwardPage((int) $pageModel->id)) {
-            throw new ResourceNotFoundException(sprintf('Page ID %s is not routable', $pageModel->id));
-        }
-    }
-
-    public function getUrlSuffixes(): array
-    {
-        return [];
-    }
-
-    private function getForwardPage(int $pageId): ?PageModel
-    {
-        $this->framework->initialize();
-
-        return PageModel::findFirstPublishedRegularByPid($pageId);
     }
 }
