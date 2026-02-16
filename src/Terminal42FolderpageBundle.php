67172a28b6b6eed7a0a1b28ed98726578827cf7f
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2026-02-16 12:56:03 +0100

    Remove obsolete stuff

--- src/Terminal42FolderpageBundle.php
+++ src/Terminal42FolderpageBundle.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace Terminal42\FolderpageBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Terminal42\FolderpageBundle\DependencyInjection\Compiler\ConfigureVoterPass;
 
 class Terminal42FolderpageBundle extends Bundle
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
-    }
-
-    public function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass(new ConfigureVoterPass());
     }
 }
